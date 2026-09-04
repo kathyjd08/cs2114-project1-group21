dd2e805bf99e46a99ba362ea733690dd75f3ad9c
annak062026S <annakinney430@gmail.com>
2026-09-04 10:30:25 -0400

    commit message

--- annakinney.java
+++ annakinney.java
@@ -1,5 +1,5 @@
 public class annakinney{
     public static void main(String[] args){
-        System.out.println("Anna Kinney is on the team.")
+        System.out.println("Anna Kinney is on the team.");
     }
 }
