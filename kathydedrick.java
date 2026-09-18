abd57a2f38a6ea50c43ee28b61562432087fbb9a
kathyjd08 <kathyjd2026@gmail.com>
2026-09-18 09:37:03 -0400

    Adding classes

--- kathydedrick.java
+++ kathydedrick.java
@@ -1,6 +1,5 @@
 public class kathydedrick {
     public static void main(String[] args) {
         System.out.println("Kathy Dedrick is on the team.");
-        System.out.println("Testing eclipse");
     }
 }
