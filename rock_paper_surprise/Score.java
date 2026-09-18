74a6ad88f8936b3beb6febb764528c47e2d8dfca
kathyjd08 <kathyjd2026@gmail.com>
2026-09-18 10:15:13 -0400

    score things

--- rock_paper_surprise/Score.java
+++ rock_paper_surprise/Score.java
@@ -1,8 +1,18 @@
 package rock_paper_surprise;
+import java.util.*;
+/**
+ *  
+ * 
+ *  @author jeand
+ *  @version Sep 18, 2026
+ */
 public class Score
 {
     //~ Fields ................................................................
-
+    public ArrayList<Player> players;
+    private String user;
+    private int score;
+    private int lives;
     //~ Constructors ..........................................................
 
     //~Public  Methods ........................................................
