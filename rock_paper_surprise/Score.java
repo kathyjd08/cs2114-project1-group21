ddf98f8464917e0c53c7e67f587482cebb0ad882
kathyjd08 <kathyjd2026@gmail.com>
2026-09-24 10:34:25 -0400

    Score class part a

--- rock_paper_surprise/Score.java
+++ rock_paper_surprise/Score.java
@@ -3,7 +3,6 @@ package rock_paper_surprise;
 import java.util.*;
 import java.io.*;
 
-
 /**
  * This class reads and writes the save file of scores and keeps track of player
  * progress.
@@ -18,24 +17,40 @@ public class Score
     private String user;
     private int score;
     private int lives;
+    private File playerRecords = new File("playerRecords.txt");
 
-    /* To Do:
-     * - save file method to make and update the file
-     * 
-     * - methods to create the arraylist of players
-     * 
-     * - method to grab game results
-     * 
-     * - get scoreboard method to give game class data for top 3 players
-     * 
-     * - the game class just needs generics to be able to give info to score and
-     * player takes info from score. 
-     * 
-     * - See persistent achievement tracker for file referencing, watching 
-     * videos on similar projects to learn how they implemented a persistent 
-     * achievement tracker
-     * 
-     * (Possibly fix my tracker so I can better answer questions)
+    public void initialize()
+    {
+        try
+        {
+            BufferedReader reader =
+                new BufferedReader(new FileReader(playerRecords));
+            String line = reader.readLine();
+            while (line != null)
+            {
+                String[] records = line.split(",");
+                int lives = Integer.parseInt(records[1]);
+                int score = Integer.parseInt(records[2]);
+                Player temp = new Player(records[0], lives, score);
+                players.add(temp);
+            }
+            reader.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * To Do: - save file method to make and update the file - methods to create
+     * the arraylist of players - method to grab game results - get scoreboard
+     * method to give game class data for top 3 players - the game class just
+     * needs generics to be able to give info to score and player takes info
+     * from score. - See persistent achievement tracker for file referencing,
+     * watching videos on similar projects to learn how they implemented a
+     * persistent achievement tracker (Possibly fix my tracker so I can better
+     * answer questions)
      */
 
 }
