3dea0419ad92a93ce5d400ebaad753cc5a14220f
kathyjd08 <kathyjd2026@gmail.com>
2026-09-24 08:52:04 -0400

    notes

--- rock_paper_surprise/Score.java
+++ rock_paper_surprise/Score.java
@@ -19,11 +19,20 @@ public class Score
 
     /* To Do:
      * - save file method to make and update the file
+     * 
      * - methods to create the arraylist of players
+     * 
      * - method to grab game results
+     * 
      * - get scoreboard method to give game class data for top 3 players
-     * - the game class just needs generics to be able to give info to score and player takes info from score. 
-     * - See persistent achievement tracker for file referencing, watching videos on similar projects to learn how they implemented a persistent achievement tracker
+     * 
+     * - the game class just needs generics to be able to give info to score and
+     * player takes info from score. 
+     * 
+     * - See persistent achievement tracker for file referencing, watching 
+     * videos on similar projects to learn how they implemented a persistent 
+     * achievement tracker
+     * 
      * (Possibly fix my tracker so I can better answer questions)
      */
 
