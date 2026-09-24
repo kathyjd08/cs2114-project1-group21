4e67351a85f372f5c63048cf8d881fc39fdedf99
kathyjd08 <kathyjd2026@gmail.com>
2026-09-24 14:15:29 -0400

    Make sure file is read

--- rock_paper_surprise/Game.java
+++ rock_paper_surprise/Game.java
@@ -57,6 +57,7 @@ public class Game
     public static void main(String[] args)
     {
         Score score = new Score();
+        score.initialize();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter username: ");
         String username = sc.nextLine();
